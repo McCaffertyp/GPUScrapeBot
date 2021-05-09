9a6896fac9490cb9eed0f2dda086a6f096a132e6
Paul <fifaplus@comcast.net>
2021-05-09 10:46:22 -0700

    Fixed Linux file split issue

--- mccaffertyp/__init__.py
+++ mccaffertyp/__init__.py
@@ -1,3 +1,13 @@
-if __file__.split('\\')[-2] != 'mccaffertyp':
-    print("Invalid file path")
-    exit(0)
+import platform
+
+system = platform.system()
+
+if system == 'Linux':
+    if __file__.split('/')[-2] != 'mccaffertyp':
+        print("Invalid file path")
+        exit(0)
+
+else:
+    if __file__.split('\\')[-2] != 'mccaffertyp':
+        print("Invalid file path")
+        exit(0)
